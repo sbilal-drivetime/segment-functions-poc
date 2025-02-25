e8be40e4cf39e26ee87fa1767a92943a9cbb0d87
Segment Git Sync <friends@segment.com>
2025-02-25 10:48:53 +0000

    Full sync

---
+++ labels/deferred_cleanup.tf
@@ -0,0 +1,10 @@
+import {
+  to = segment_label.id-deferred_cleanup
+  id = "deferred:cleanup"
+}
+
+resource "segment_label" "id-deferred_cleanup" {
+  description = null
+  key         = "deferred"
+  value       = "cleanup"
+}
---
+++ labels/org_bridgecrest.tf
@@ -0,0 +1,10 @@
+import {
+  to = segment_label.id-org_bridgecrest
+  id = "org:bridgecrest"
+}
+
+resource "segment_label" "id-org_bridgecrest" {
+  description = null
+  key         = "org"
+  value       = "bridgecrest"
+}
