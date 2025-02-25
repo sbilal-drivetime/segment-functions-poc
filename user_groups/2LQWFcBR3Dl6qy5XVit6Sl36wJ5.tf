d603d5cd2c9c7943494538063dd9a156f490bdd4
Segment Git Sync <friends@segment.com>
2025-02-25 10:49:03 +0000

    Full sync

---
+++ user_groups/2LQWFcBR3Dl6qy5XVit6Sl36wJ5.tf
@@ -0,0 +1,88 @@
+import {
+  to = segment_user_group.id-2LQWFcBR3Dl6qy5XVit6Sl36wJ5
+  id = "2LQWFcBR3Dl6qy5XVit6Sl36wJ5"
+}
+
+resource "segment_user_group" "id-2LQWFcBR3Dl6qy5XVit6Sl36wJ5" {
+  members = ["adam.hammond@drivetime.com", "aditi.sharma@drivetime.com", "adnan.raza@drivetime.com", "alexander.rotteveel@drivetime.com", "andrew.carlson@silverrockinc.com", "anjani.devi@drivetime.com", "aparna.yellapragada@bridgecrest.com", "arpita.ojha@drivetime.com", "arsalan.khan@drivetime.com", "ayesha.khatoon@drivetime.com", "brent.blaylock@bridgecrest.com", "brian.burke1@drivetime.com", "brian.hickey@drivetime.com", "chi.zama@drivetime.com", "chris.werley@drivetime.com", "cody.stone@bridgecrest.com", "daniel.folz@drivetime.com", "daniel.verhagen@drivetime.com", "david.aldridge@drivetime.com", "david.oesterreich@bridgecrest.com", "dinesh.ameti@drivetime.com", "elyse.wells@drivetime.com", "ernesto.rendon@drivetime.com", "faraz.haroon@drivetime.com", "girish.mittal@drivetime.com", "gregory.lee@drivetime.com", "gwendolyn.lukemire@drivetime.com", "haonan.wang@go-fi.com", "harley.sweigart@drivetime.com", "ian.foutz@drivetime.com", "jacob.meline@drivetime.com", "james.green@drivetime.com", "jeremy.johnson@drivetime.com", "johnathan.townsley@drivetime.com", "jonathan.little@bridgecrest.com", "jonathan.oberhaus@drivetime.com", "joseph.jacobs1@drivetime.com", "juan.lopez1@drivetime.com", "juliana.ramos@drivetime.com", "jyoti.singh@drivetime.com", "kari.freidus@drivetime.com", "kartikay.kaushik@drivetime.com", "keenan.steele@drivetime.com", "ketaki.madkikar@drivetime.com", "kristin.beaty@drivetime.com", "logan.patterson@drivetime.com", "manas.choudhary@drivetime.com", "matthew.jones@drivetime.com", "michael.burroughs@drivetime.com", "mohammad.hassan@drivetime.com", "moshiur.rahman@drivetime.com", "nagabhavya.amaravadi@drivetime.com", "nara.alzapur@drivetime.com", "nate.warner@drivetime.com", "nathan.griffin@go-fi.com", "nayana.mallikarjuna@drivetime.com", "nitan.rana@drivetime.com", "peter.grisolano@drivetime.com", "qasim.khan@drivetime.com", "rajendar.gurram@drivetime.com", "richard.nuckolls@drivetime.com", "shehzad.bilal@drivetime.com", "shilpa.reddymekapothula@drivetime.com", "shwetha.cheruku@drivetime.com", "sony.philip@drivetime.com", "srinivas.chevali@drivetime.com", "steven.toth@drivetime.com", "subodh.raysoni@drivetime.com", "suryanandsagar.kukkala@drivetime.com", "syed.waliullah@drivetime.com", "sylvia.chung@drivetime.com", "tanmay.sharma@drivetime.com", "umer.muhammad@drivetime.com", "umesh.katkar@drivetime.com", "vaibhav.sathe@drivetime.com", "venuniranjanchowdary.machavarapu@drivetime.com", "vibhuti.patel@drivetime.com", "victor.ifarinde@drivetime.com", "vijayadithya.doddi@drivetime.com", "vivek.shah@drivetime.com", "waleed.naveed@drivetime.com", "waseem.bakhsh@drivetime.com", "wei.stafford@drivetime.com", "yokeshwaran.lokanathan@drivetime.com", "yonas.reda@drivetime.com", "yousuf.khan@bridgecrest.com"]
+  name    = "RAC-SEGMENT-DEVELOPER"
+  permissions = [
+    {
+      resources = [
+        {
+          id = "tJXJx41ERuqHR7rejkf5ZZ"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1Mc8DEHhMqqSkb5cPjtu9pAEyNf"
+    },
+    {
+      resources = [
+        {
+          id = "tJXJx41ERuqHR7rejkf5ZZ"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1Mc8DFJ5XdtkAaokYZCgoiUr6LJ"
+    },
+    {
+      resources = [
+        {
+          id = "tJXJx41ERuqHR7rejkf5ZZ"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1Mc8DHU42rYyIp94oBPexlvYpf8"
+    },
+    {
+      resources = [
+        {
+          id = "tJXJx41ERuqHR7rejkf5ZZ"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1Mc8DKqCj3vrDvNml9Vhsc7Aebt"
+    },
+    {
+      resources = [
+        {
+          id = "tJXJx41ERuqHR7rejkf5ZZ"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1PWCliYHNGD6q2xVM3MjSCD5WRU"
+    },
+    {
+      resources = [
+        {
+          id = "tJXJx41ERuqHR7rejkf5ZZ"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1Ttjwdg1SEGKH5hfg8heUBlfXGK"
+    },
+    {
+      resources = [
+        {
+          id = "tJXJx41ERuqHR7rejkf5ZZ"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1dsSKGeBDUTGJ1QoZTtymZq949J"
+    },
+  ]
+}
---
+++ user_groups/2LQWFjjlq2OCODestDLiHKO2Yfs.tf
@@ -0,0 +1,22 @@
+import {
+  to = segment_user_group.id-2LQWFjjlq2OCODestDLiHKO2Yfs
+  id = "2LQWFjjlq2OCODestDLiHKO2Yfs"
+}
+
+resource "segment_user_group" "id-2LQWFjjlq2OCODestDLiHKO2Yfs" {
+  members = ["aaron.mciver@drivetime.com", "adam.zeffer@drivetime.com", "akshay.golash@drivetime.com", "aparna.yellapragada@bridgecrest.com", "brent.blaylock@bridgecrest.com", "brian.burke1@drivetime.com", "chi.zama@drivetime.com", "dan.pousson@drivetime.com", "david.oesterreich@bridgecrest.com", "derrik.simmons@bridgecrest.com", "gregory.lee@drivetime.com", "gwendolyn.lukemire@drivetime.com", "harley.sweigart@drivetime.com", "jeremy.johnson@drivetime.com", "joseph.jacobs1@drivetime.com", "justin.miller@drivetime.com", "lisa.aguilera@drivetime.com", "saurabh.gupta@bridgecrest.com", "shehzad.bilal@drivetime.com", "srinivas.chevali@drivetime.com", "steven.cox@drivetime.com", "yokeshwaran.lokanathan@drivetime.com", "zachary.johnson@bridgecrest.com"]
+  name    = "RAC-SEGMENT-ADMIN"
+  permissions = [
+    {
+      resources = [
+        {
+          id = "tJXJx41ERuqHR7rejkf5ZZ"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1Mc8DFMmZbyzdEWmE6k8JVgKAiE"
+    },
+  ]
+}
